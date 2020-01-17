7abd8ab5d131407d40f74f5c471df461e97e3c6d
ShaigroRB <why_not.shaigro@yahoo.fr>
2020-01-17 11:52:57 +0100

    :recycle: changed plain text to use class for citations (more options)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
+class Citation {
+    constructor(text, author, title = "") {
+        this.text = text ;
+        this.author = author ;
+        this.title = title ;
+    }
+
+    getFormalised() {
+        var _span = `<i>${this.author}</i></span>` ;
+        if (this.title) {
+            _span = `<span title="${this.title}>${_span}` ;
+        }
+        else {
+            _span = `<span>${_span}` ;
+        }
+        return `"${this.text}" - ${_span}` ;
+    }
+}
+
 citations = [
-                "Hope for the best, plan for the worst. - Lee Child", 
-                "You don't have to be great to start, but you have to start to be great. - Zig Ziglar",
-                "The best time to plant a tree is twenty five years ago. The second best time is now.",
-                "Music discovered by accident has the most dopamine. - Paul Oketch (a truthful commenter on youtube)",
-                "La moitié d'un ami, c'est la moitié d'un traitre. - Victor Hugo"                
+                new Citation("Hope for the best, plan for the worst.", "Lee Child")                
             ] ;
 len_citations = citations.length ;
 
 footer_citations = document.getElementById("footer-citations") ;
 {
     rnd = Math.floor(Math.random() * len_citations) ;
-    footer_citations.innerText = citations[rnd] ;
+    footer_citations.innerHTML = citations[rnd].getFormalised() ;
 } ;
